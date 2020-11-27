1337942e4f6557d60cb26fbc04ad393c34218dd6
ivgtr <gear.1112t.o@gmail.com>
2020-11-27 18:56:11 +0900

    :sparkles: add: driveにファイルをアップロードする機能

--- src/utils/saveToData.ts
+++ src/utils/saveToData.ts
@@ -1,23 +1,9 @@
 import { google } from 'googleapis'
 import * as dotenv from 'dotenv'
+import { PassThrough } from 'stream'
+import fetch from 'node-fetch'
+import fileType from 'file-type'
 import creds from '../jsons/client_secret.json'
-
-const sample: media[] = [
-  {
-    tweet_time: '2019/11',
-    tweet_url: 'https://',
-    file_name: 'hoge2',
-    media_type: 'photo',
-    media_url: 'https://'
-  },
-  {
-    tweet_time: '2019/11',
-    tweet_url: 'https://',
-    file_name: 'foo2',
-    media_type: 'video',
-    media_url: 'https://'
-  }
-]
 
 dotenv.config()
 
@@ -49,11 +35,58 @@ const drive = google.drive({
   auth: jwtClient
 })
 
-const saveDriveData = async (resources: string[]) => {
+const saveDriveData = async (resources: resources[]) => {
   try {
-    drive.files.create({})
+    resources.map(async (data) => {
+      const bufferData: {
+        data: Buffer
+        mime: string
+      } = await new Promise((resolve, reject) => {
+        fetch(data.media_url, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json; charset=utf-8'
+          }
+        })
+          .then((response) => response.buffer())
+          .then(async (result) => {
+            const { mime } = (await fileType.fromBuffer(result)) as {
+              ext: string
+              mime: string
+            }
+            return resolve({
+              data: result,
+              mime
+            })
+          })
+          .catch((err) => {
+            return reject(err)
+          })
+      })
+
+      const bufferStream = new PassThrough()
+      bufferStream.end(bufferData.data)
+
+      drive.files.create(
+        {
+          requestBody: {
+            parents: [folderId as string],
+            mimeType: bufferData.mime,
+            name: data.file_name
+          },
+          media: {
+            mimeType: bufferData.mime,
+            body: bufferStream
+          },
+          fields: 'id'
+        },
+        {}
+      )
+
+      return data
+    })
   } catch (error) {
-    console.log(`The API returned an error: ${error}`)
+    console.log(`The Drive API returned an error: ${error}`)
   }
 }
 const setSheetData = async (values: string[][]) => {
@@ -76,9 +109,12 @@ const setSheetData = async (values: string[][]) => {
 
 const getSheetRequest = async (
   data: media[]
-): Promise<{ values: string[][]; resources: string[] }> => {
+): Promise<{
+  values: string[][]
+  resources: resources[]
+}> => {
   const values: string[][] = []
-  const resources: string[] = []
+  const resources: resources[] = []
 
   try {
     const responseGetSheet = await sheets.spreadsheets.values.get({
@@ -90,32 +126,46 @@ const getSheetRequest = async (
     data.map((media) => {
       if (!names.includes(media.file_name)) {
         values.push([
-          media.tweet_time,
           media.tweet_url,
+          media.tweet_time,
           media.file_name,
           media.media_type,
-          media.media_url
+          media.media_url,
+          media.save_time
         ])
-        resources.push(media.media_url)
+        resources.push({
+          media_url: media.media_url,
+          file_name: media.file_name,
+          media_extension: media.media_url.slice(
+            media.media_url.lastIndexOf('.') + 1
+          )
+        })
       }
       return media
     })
   } catch (error) {
-    console.log(`The API returned an error: ${error}`)
+    console.log(`The Sheet API returned an error: ${error}`)
   }
 
   return { values, resources }
 }
 
 const saveToData = async (data: media[]): Promise<boolean> => {
-  const { values, resources } = await getSheetRequest(data)
-  if (values.length) {
-    await Promise.all([setSheetData(values), saveDriveData(resources)])
-      .then(() => console.log('全部終わりました！'))
-      .catch((error) => console.log(error))
+  try {
+    const { values, resources } = await getSheetRequest(data)
+    if (values.length) {
+      await Promise.all([setSheetData(values), saveDriveData(resources)]).catch(
+        (error) => {
+          throw new Error(error)
+        }
+      )
+    }
+    console.log(`${resources.length}件追加`)
     return true
+  } catch (error) {
+    console.log(`Error: ${error}`)
+    return false
   }
-  return false
 }
 
 export default saveToData
