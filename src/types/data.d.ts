cfb5ee330057e6a8d2b2aff81d2083ffd58bccbd
ivgtr <gear.1112t.o@gmail.com>
2020-11-27 18:43:46 +0900

    :pencil2: add: 型を追加

--- src/types/data.d.ts
+++ src/types/data.d.ts
@@ -1,13 +1,20 @@
 type media = {
-  tweet_time: string
   tweet_url: string
+  tweet_time: string
   file_name: string
   media_type: string
   media_url: string
+  save_time: string
 }
 
 type VideoVariant = {
   bitrate?: number | null
   content_type: string
   url: string
+}
+
+type resources = {
+  media_url: string
+  file_name: string
+  media_extension: string
 }
